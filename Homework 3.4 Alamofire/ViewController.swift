d379d381723e93fae9b04e748453e78ae9b4f563
Tatarax <dinarvaf@icloud.com>
2023-03-21 21:16:32 +0300

    Initial Commit

---
+++ Homework 3.4 Alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 3.4 Alamofire
+//
+//  Created by Dinar on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
